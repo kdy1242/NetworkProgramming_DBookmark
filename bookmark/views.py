5a52dc9fcef0dd11f82ef12715f332869db0315a
kdy1242 <dy12311229@gmail.com>
2022-06-13 14:48:04 +0900

    bookmark/views BookmarkListView get_queryset(), BookmarkCreateView get_initial()

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
+from accounts.models import Profile
 from bookmark.models import Bookmark
 
 
 class BookmarkListView(ListView):
     model = Bookmark
+
     # bookmark_list.html, {'bookmark_list' : Bookmark.object.all()}
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:   # 로그인 하면, 로그인한 사용자의 북마크만 보이자
+            # user -> profile -> bookmark
+            profile = Profile.objects.get(user=user)    # user -> profile
+            bookmark_list = Bookmark.objects.filter(profile=profile)    # profile -> bookmark_list
+        else:                       # 로그인 안하면
+            # bookmark_list = Bookmark.objects.all()     # 북마크 다 보여주자
+            bookmark_list = Bookmark.objects.none()    # 북마크 보이지 말자
+        return bookmark_list
 
 
 class BookmarkCreateView(CreateView):
     model = Bookmark
-    fields = ['name', 'url']    # '__all__'
-    template_name_suffix = '_create'     # bookmark_form.html -> bookmark_create.html
+    fields = ['profile', 'name', 'url']  # '__all__'
+    template_name_suffix = '_create'  # bookmark_form.html -> bookmark_create.html
     success_url = reverse_lazy('bookmark:list')
+
+    def get_initial(self):
+        # user -> profile
+        user = self.request.user
+        profile = Profile.objects.get(user=user)
+        return {'profile': profile}
 
 
 class BookmarkDetailView(DetailView):
@@ -23,8 +41,8 @@ class BookmarkDetailView(DetailView):
 
 class BookmarkUpdateView(UpdateView):
     model = Bookmark
-    fields = ['name', 'url']    # '__all__'
-    template_name_suffix = '_update'    # bookmark_update.html
+    fields = ['name', 'url']  # '__all__'
+    template_name_suffix = '_update'  # bookmark_update.html
     # success_url = reverse_lazy('bookmark:list')     # success_url 없으면 model 의 get_absolute_url() 호출
 
 
